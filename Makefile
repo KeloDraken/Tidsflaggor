a9c3ef044e0daa051ea877b04b93dc04af18a51c
Vuyo <joymama078@gmail.com>
2024-03-29 23:42:56 +0200

    Setup docker project

--- Makefile
+++ Makefile
@@ -2,13 +2,6 @@ SOURCE_DIR=src
 CORE_DIR=$(SOURCE_DIR)/core
 ARGS ?=
 
-.PHONY: project_setup
-project_setup:
-	@echo "Setting up project..."
-	@echo "Installing dependencies..."
-	@pip install -r requirements.txt
-	@echo "Project setup complete."
-
 .PHONY: startapp
 startapp:
 	@echo "Creating app..."
@@ -20,3 +13,17 @@ migrate:
 	@echo "Migrating database..."
 	@cd $(SOURCE_DIR) && python manage.py migrate $(ARGS)
 	@echo "Database migrated."
+
+
+.PHONY: runserver
+runserver:
+	@echo "Migrating database..."
+	@python manage.py migrate
+	@echo "Database migrated."
+
+	@echo "Collecting static files..."
+	@python manage.py collectstatic --noinput
+	@echo "Static files collected."
+
+	@echo "Starting server..."
+	@python manage.py runserver 0.0.0.0:8000
--- src/config/settings/development.py
+++ src/config/settings/development.py
@@ -1,32 +1,40 @@
 """
 Django development settings for src project.
 """
+
+import os
+
 from config.settings.base import *
 
-SECRET_KEY = 'django-insecure-i_+ydg8wuu6yg&xlb$q7_$(b*b$+$&e3hp%qzli0dsp6*-@5oj'
+SECRET_KEY = "django-insecure-i_+ydg8wuu6yg&xlb$q7_$(b*b$+$&e3hp%qzli0dsp6*-@5oj"
 
 ALLOWED_HOSTS = []
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR.parent / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "flaggor_db"),
+        "USER": os.getenv("POSTGRES_USER", "flaggor_user"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "flaggor_password"),
+        "HOST": os.getenv("POSTGRES_HOST", "flaggor_db"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static-local"
