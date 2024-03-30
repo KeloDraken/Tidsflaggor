5e5a22b836d7b734dcd584df5024744cdf292edc
Vuyo <joymama078@gmail.com>
2024-03-30 09:08:48 +0200

    Begin work on quizzes app

--- src/config/urls.py
+++ src/config/urls.py
@@ -8,11 +8,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = (
     [
         path("admin/", admin.site.urls),
+        # Quizzes
+        path("quiz/", include("core.quiz.urls", namespace="quiz")),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/core/quiz/views.py
+++ src/core/quiz/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_quiz(request: HttpRequest, quiz_id: str) -> HttpResponse:
+    return render(request, "quizzes/quiz.html", {"quiz_id": quiz_id})
